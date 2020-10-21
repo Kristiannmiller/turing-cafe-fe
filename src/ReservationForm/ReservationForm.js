dd6d53d1783219086ead293c8da0b19df68d942c
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-21 10:34:16 -0600

    Add input types and placeholders to render

--- src/ReservationForm/ReservationForm.js
+++ src/ReservationForm/ReservationForm.js
@@ -13,11 +13,11 @@ class ReservationForm extends Component {
   }
   render() {
     return (
-      <form>
-        <input/>
-        <input/>
-        <input/>
-        <input/>
+      <form onChange={this.handleChange}>
+        <input type="text" placeholder="name"/>
+        <input type="text" placeholder="date mm/dd"/>
+        <input type="text" placeholder="time h:mm"/>
+        <input type="number" min="1" placeholder="num. of guests"/>
         <button>reserve</button>
       </form>
     )
