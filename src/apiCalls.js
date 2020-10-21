e023f59d64363984fbdaffc59f2239984ecfddc6
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-21 11:07:22 -0600

    Create postReservation method for posting a new reservation to the api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,6 +4,18 @@ const apiCalls = {
       .then(response => response.json())
       .then(data => data)
       .catch(error => error)
+  },
+  postReservation(resInfo) {
+    return fetch(`http://localhost:3001/api/v1/reservations`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(resInfo)
+    })
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => err.message)
   }
 }
 export default apiCalls;
