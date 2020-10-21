bd7290999e294aff227946f09771886077916ae3
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-21 09:59:52 -0600

    Create initial ReservationCard functional component files

---
+++ src/ReservationCard/ReservationCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ReservationCard.css';
+
+const ReservationCard = ( reservations ) => {
+  return (
+    <section className="resCard">
+      <h1>name:</h1>
+      <h2>date:</h2>
+      <h3>time:</h3>
+      <h4>guests:</h4>
+    </section>
+  )
+}
