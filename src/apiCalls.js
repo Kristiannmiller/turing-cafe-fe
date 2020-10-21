904f05b9492e7b9507ba1427db2ad6f0197ac56e
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-21 09:51:40 -0600

    Add getAllReservations method to apiCalls to get initial data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,9 @@
 const apiCalls = {
-
+  getAllReservations() {
+    return fetch(`http://localhost:3001/api/v1/reservations`)
+      .then(response => response.json())
+      .then(data => data)
+      .catch(error => error)
+  }
 }
 export default apiCalls;
