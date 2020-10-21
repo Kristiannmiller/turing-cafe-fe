6fe875cdfd2ded0eb1afdc24cf7b1b5b01772abd
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-21 12:09:33 -0600

    Start test for firing handleChange function - FAIL

--- src/ReservationForm/ReservationForm.test.js
+++ src/ReservationForm/ReservationForm.test.js
@@ -3,15 +3,15 @@ import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import ReservationForm from './ReservationForm.js';
+import { handleChange } from './ReservationForm.js';
 import { BrowserRouter } from 'react-router-dom';
+// jest.mock('./ReservationForm.js');
 
 describe('ReservationForm', () => {
   it('should render a reservation card', () => {
-    const testRes = {id: 1, name: 'Kristi', date: '10/21', time: '7:00', number: 3 }
     render(
       <BrowserRouter>
-        <ReservationForm
-        />
+        <ReservationForm />
       </BrowserRouter>
     );
 
@@ -19,5 +19,22 @@ describe('ReservationForm', () => {
     expect(screen.getByPlaceholderText('date mm/dd')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('time h:mm')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('num. of guests')).toBeInTheDocument();
+    expect(screen.getByText('Make Reservation')).toBeInTheDocument();
   })
+
+  // it('should fire a function for every input change', () => {
+  //   const fakeHandleChange = jest.mock(handleChange)
+  //   render(
+  //     <BrowserRouter>
+  //       <ReservationForm />
+  //     </BrowserRouter>
+  //   );
+  //   const nameInput = waitFor(() => screen.getByPlaceholderText('name'))
+  //   userEvent.type(nameInput, 'Kristi');
+  //   expect(fakeHandleChange).toHaveBeenCalledTimes(6)
+  //   userEvent.type(screen.getByPlaceholderText('date mm/dd'), '10/22');
+  //   userEvent.type(screen.getByPlaceholderText('time h:mm'), '8:00');
+  //   userEvent.type(screen.getByPlaceholderText('num. of guests'), '4');
+  //
+  // })
 })
